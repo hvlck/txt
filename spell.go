e74954fab6076aba1ede1c4bb20b77f14be1ce11
hvlck <just.ethan@protonmail.com>
2022-07-09 20:07:20 -0400

    feat: add basic spellcheck, levenshtein distance calculations

---
+++ spell.go
@@ -0,0 +1,130 @@
+// levenshtein distance
+package txt
+
+import (
+	"bytes"
+	"io/ioutil"
+)
+
+// loads the dictionary words list
+func load() ([][]byte, error) {
+	b, err := ioutil.ReadFile("./words.txt")
+	if err != nil {
+		return nil, nil
+	}
+
+	return bytes.Split(b, []byte("\n")), nil
+}
+
+var dict, dictErr = load()
+
+// generates a list of spelling corrections for the provided `word`
+// `lim` is the maximum levenshtein distance away for a correction to be returned (inclusive)
+// e.g. a correction with a LD of 3 would be returned with a limit of `3`, but a word with a LD of 4 would not
+// in the return values, the `uint8` in the map corresponds to levenshtein distance of the corrected word
+// todo: better, more focused results; current implementation returns many options, especially for smaller words
+// could possibly be fixed by weighting spelling errors that are closer on keyboards
+// e.g. with the input `vad`
+// `tad` and `bad` are both options, but the "b" in `bad` is closer physically on the keyboard than the "t" in
+// `tab`, and so would be the better choice
+func Correct(word string, lim uint8) (map[string]uint8, error) {
+	if dictErr != nil {
+		return nil, dictErr
+	}
+
+	// all found matches
+	matches := map[string]uint8{}
+
+	for i := 0; i < len(dict); i++ {
+		// levenshtein distance of correction
+		l := Ld(word, string(dict[i]))
+		if l <= lim {
+			matches[string(dict[i])] = l
+			lim = l
+		}
+	}
+
+	return matches, nil
+}
+
+// returns the minimum of a function
+func min(v ...uint8) uint8 {
+	m := v[0]
+
+	for _, k := range v {
+		if k < m {
+			m = k
+		}
+	}
+
+	return m
+}
+
+// levenshtein distance
+// based in part on https://rosettacode.org/wiki/Levenshtein_distance#Go, some modifications made to use one-dimensional array
+// this version usually takes about half the time as the second version, and usually less than half the time of the first version on RosettaCode
+func Ld(a, b string) uint8 {
+	if a == "" {
+		return uint8(len(b))
+	}
+	if b == "" {
+		return uint8(len(a))
+	}
+	if a == b {
+		return 0
+	}
+
+	// row is the previous row in the LD table (contains top right at current index and top left at current index - 1)
+	row := make([]uint8, len(a)+1)
+	for i := range row {
+		row[i] = uint8(i)
+	}
+
+	// first characters aren't the same
+	var current uint8
+
+	// bottom left, starts at 1
+	var bl uint8
+	// go through columns first
+	for i := 1; i <= len(b); i++ {
+		// previous top left - used for if letters are the same
+		ptl := uint8(i - 1)
+		// set first value of previous row equal to ptl
+		row[0] = ptl
+		current = 0
+
+		// top left
+		var tl uint8
+		// top right
+		var tr uint8
+		bl = uint8(i)
+
+		// go through each character in the row
+		for j := 1; j <= len(a); j++ {
+			// set top right equal to the value at
+			tr = row[j]
+			tl = ptl
+
+			// in first row of array, so top values should be equal to index of item (e.g. [0 1 2 3 4 5])
+			// value of top right should then be the value of the array at the index in the current loop
+			if i == 1 {
+				tr = uint8(j)
+			}
+
+			// characters are the same - use previous top left value
+			if a[j-1] == b[i-1] {
+				current = tl
+			} else {
+				// characters are different - take minimum of the three, add one operation
+				current = min(tl, tr, bl) + 1
+			}
+
+			// set the previous top left value equal to
+			ptl = row[j]
+			row[j] = current
+			bl = current
+		}
+	}
+
+	return uint8(current)
+}
---
+++ spell_test.go
@@ -0,0 +1,124 @@
+package txt
+
+import (
+	"testing"
+)
+
+// rt and ro() https://rosettacode.org/wiki/Levenshtein_distance#Go
+func rt(s, t string) int {
+	d := make([][]int, len(s)+1)
+	for i := range d {
+		d[i] = make([]int, len(t)+1)
+	}
+	for i := range d {
+		d[i][0] = i
+	}
+	for j := range d[0] {
+		d[0][j] = j
+	}
+	for j := 1; j <= len(t); j++ {
+		for i := 1; i <= len(s); i++ {
+			if s[i-1] == t[j-1] {
+				d[i][j] = d[i-1][j-1]
+			} else {
+				min := d[i-1][j]
+				if d[i][j-1] < min {
+					min = d[i][j-1]
+				}
+				if d[i-1][j-1] < min {
+					min = d[i-1][j-1]
+				}
+				d[i][j] = min + 1
+			}
+		}
+
+	}
+	return d[len(s)][len(t)]
+}
+
+func ro(s, t string) int {
+	if s == "" {
+		return len(t)
+	}
+	if t == "" {
+		return len(s)
+	}
+	if s[0] == t[0] {
+		return ro(s[1:], t[1:])
+	}
+	a := ro(s[1:], t[1:])
+	b := ro(s, t[1:])
+	c := ro(s[1:], t)
+	if a > b {
+		a = b
+	}
+	if a > c {
+		a = c
+	}
+	return a + 1
+}
+
+const one = "avid"
+const two = "antidisestablishmentarianism"
+
+// system specs:
+// M1 8-core Macbook Air, 16GB RAM
+// benchmark ran on July 9 2022
+// latest commit was https://github.com/hvlck/txt/commit/66a24f9329b2d6bd8c9ff496314cc2bcca9aed49
+
+// go test -bench=. -benchmem -benchtime=1x
+// BenchmarkLd/rosetta_code_loop-8         	       1	      3333 ns/op	    1328 B/op	       6 allocs/op
+// BenchmarkLd/rosetta_code_slice-8        	       1	     31292 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkLd/txt-8                       	       1	      1000 ns/op	      16 B/op	       1 allocs/op
+// BenchmarkSpellcheck-8                   	       1	  10183583 ns/op	  339536 B/op	   84157 allocs/op
+
+// go test -bench=. -benchmem -benchtime=100x
+// BenchmarkLd/rosetta_code_loop-8         	     100	        89.16 ns/op	      13 B/op	       0 allocs/op
+// BenchmarkLd/rosetta_code_slice-8        	     100	       317.9 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkLd/txt-8                       	     100	         8.330 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkSpellcheck-8                   	     100	    100943 ns/op	    3393 B/op	     841 allocs/op
+
+// go test -bench=. -benchmem -benchtime=1000000x
+// BenchmarkLd/rosetta_code_loop-8         	 1000000	         0.008833 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkLd/rosetta_code_slice-8        	 1000000	         0.03225 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkLd/txt-8                       	 1000000	         0.0007500 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkSpellcheck-8                   	 1000000	        10.12 ns/op	       0 B/op	       0 allocs/op
+func BenchmarkLd(b *testing.B) {
+	b.Run("rosetta code loop", func(b *testing.B) {
+		rt(one, two)
+		b.StopTimer()
+	})
+
+	b.Run("rosetta code slice", func(b *testing.B) {
+		ro(one, two)
+		b.StopTimer()
+	})
+
+	b.Run("txt", func(b *testing.B) {
+		Ld(one, two)
+		b.StopTimer()
+	})
+}
+
+func TestLd(t *testing.T) {
+	f := Ld(one, two)
+	if f != 25 {
+		t.Fail()
+	}
+}
+
+func TestSpellcheck(t *testing.T) {
+	_, err := Correct("wat", 3)
+	if err != nil {
+		t.Fail()
+	}
+
+}
+
+func BenchmarkSpellcheck(b *testing.B) {
+	_, err := Correct("wat", 3)
+	b.StopTimer()
+	if err != nil {
+		b.Fail()
+	}
+}
