4d013ee71347b544f1930c76399062dac1032897
hvlck <just.ethan@protonmail.com>
2022-08-08 18:52:21 -0400

    feat: accurate word counting and splitting

--- words.go
+++ words.go
@@ -7,11 +7,37 @@ var READING_WPM = 200
 
 // Duration, in seconds, to read the provided string `s`.
 func ReadTime(s string) int {
-	return len(strings.Split(s, " ")) / READING_WPM
+	return len(Words(s)) / READING_WPM
 }
 
 func WordCount(s string, spaces bool) int {
-	return len(strings.Split(s, " "))
+	return len(Words(s))
+}
+
+func replaceSpaces(s string) string {
+	s = strings.ReplaceAll(s, "  ", " ")
+	if strings.Contains(s, "  ") {
+		return replaceSpaces(s)
+	}
+
+	return s
+}
+
+func removeChars(s string, characters ...string) string {
+	for _, v := range characters {
+		s = strings.ReplaceAll(s, v, "")
+	}
+	return s
+}
+
+// Returns the individual words in a string.
+// Also removes punctuation (".", ",", ":", ";", "!", "?"). Parantheses are kept, as well as brackets and quotation
+// marks.
+func Words(s string) []string {
+	s = replaceSpaces(s)
+	s = strings.ReplaceAll(s, "-", " ")
+	s = removeChars(s, ".", ",", ":", ";", "!", "?")
+	return strings.Split(s, " ")
 }
 
 func CharCount(s string) int {
--- words_test.go
+++ words_test.go
@@ -13,3 +13,25 @@ func TestReadTime(t *testing.T) {
 		}
 	}
 }
+
+func TestWords(t *testing.T) {
+	examples := []string{
+		"The   quick  brown fox jumped over the   sly dog",
+		"the-rest-of-the-word",
+		"This contains two sentences. This is the second!",
+	}
+
+	words := [][]string{
+		{"The", "quick", "brown", "fox", "jumped", "over", "the", "sly", "dog"},
+		{"the", "rest", "of", "the", "word"},
+		{"This", "contains", "two", "sentences", "This", "is", "the", "second"},
+	}
+
+	for idx, v := range examples {
+		for index, word := range Words(v) {
+			if words[idx][index] != word {
+				t.Fatalf("expected %v, got %v", words[idx][index], word)
+			}
+		}
+	}
+}
