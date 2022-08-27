c930cdd9d0895e9bf07fd07f7d34ebca827a2091
hvlck <just.ethan@protonmail.com>
2022-08-27 18:29:27 -0400

    feat: default normalizer, basic normalizer functions, tests

--- normalize.go
+++ normalize.go
@@ -8,6 +8,43 @@ import (
 // Normalizes a given string.
 type Normalizer func(text string) string
 
+var (
+	// Converts all characters to lowercase.
+	NormalizerToLower Normalizer = func(text string) string { return strings.ToLower(text) }
+
+	// Converts excess whitespace into a single space.
+	NormalizeSingleSpace Normalizer = func(text string) string {
+		return replaceSpaces(text)
+	}
+
+	// Removes most punctuation. Periods and slashes are not removed.
+	NormalizePunctuation Normalizer = func(text string) string {
+		return removeChars(text, ",", ":", ";", "!", "?", "\"", "'", "(", ")", "&")
+	}
+
+	// Removes all punctuation, including periods and slashes.
+	NormalizeAllPunctuation Normalizer = func(text string) string {
+		return removeChars(NormalizePunctuation(text), ".", "/", "\\")
+	}
+
+	NormalizeSpecial Normalizer = func(text string) string {
+		return removeChars(text, "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "]", "{", "}", ";", ":", "'", "\"", "<", ">", "?", "/", "\\", "~", "`")
+	}
+
+	// Replaces instances of hyphens with spaces.
+	NormalizeHyphens Normalizer = func(text string) string {
+		return strings.ReplaceAll(text, "-", " ")
+	}
+)
+
+// Default normalizer used if no normalizers are provided.
+var DefaultNormalizer = []Normalizer{
+	NormalizerToLower,
+	NormalizePunctuation,
+	NormalizeSingleSpace,
+	NormalizeHyphens,
+	NormalizeSpecial,
+}
 
 // Normalizes a given string using the provided options. Options are executed in the order that they're provided in.
 // If no options are provided, the default normalizer is used.
--- normalize_test.go
+++ normalize_test.go
@@ -2,3 +2,26 @@ package txt
 
 import "testing"
 
+func TestNormalize(t *testing.T) {
+	tests := []string{
+		"A  ",
+		"!",
+		"The quick brown fox jumped over the sly  dog.",
+		"!@#$%^&*()-+=_[]{}\"';:<>?/\\.,`~",
+	}
+
+	expected := []string{
+		"a ",
+		"",
+		"the quick brown fox jumped over the sly dog.",
+		" .",
+	}
+
+	for idx, v := range tests {
+		n := Normalize(v)
+
+		if n != expected[idx] {
+			t.Fatalf("expected '%v', got '%v' in '%v'", expected[idx], n, v)
+		}
+	}
+}
