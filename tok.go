b337f9b83a30c0b8708df02bc7793110eca074ca
hvlck <just.ethan@protonmail.com>
2022-08-27 20:07:27 -0400

    feat: color normalization tokenizer, replacement dictionary loader

--- tok.go
+++ tok.go
@@ -8,6 +8,43 @@ import (
 	"unicode"
 )
 
+// Loads a replacer file from a given path.
+// Files must be in the format:
+// original,replaced value
+// with each tuple on a new line.
+// The values are loaded into memory; the replaced value is assigned the `Data` field on the final
+// node of a trie branch, which can be accessed using Trie.NodeAt(x).
+func LoadReplacer(path string, caseSensitive bool) *Node {
+	f, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	t := NewTrie()
+
+	for _, v := range bytes.Split(f, []byte("\n")) {
+		tuple := bytes.Split(v, []byte(","))
+		if len(tuple) < 2 {
+			break
+		}
+
+		original := tuple[0]
+		if !caseSensitive {
+			original = bytes.ToLower(original)
+		}
+		replaced := tuple[1]
+
+		t.Insert(string(original), replaced)
+	}
+
+	return t
+}
+
+var (
+	// English colors and their hex equivalents.
+	Colors *Node = LoadReplacer("./dicts/colors.txt", false)
+)
+
 // Normalizes a list of tokens.
 type Tokenizer func(tokens []string) []string
 
@@ -17,6 +54,17 @@ var (
 	// Stems tokens using a Porter stemmer.
 	TokenizerStemmer Tokenizer = func(tokens []string) []string { return StemTokens(tokens) }
 
+	// Normalizes color names (e.g. `red`, `navy`) into their hex values.
+	TokenizerColors Tokenizer = func(tokens []string) []string {
+		for i, v := range tokens {
+			if n, exists := Colors.At(v); exists {
+				tokens[i] = string(n.Data)
+			}
+		}
+
+		return tokens
+	}
+)
 
 var (
 	// Splits a string at non-alphanumeric characters (whitespace, punctuation, etc).
